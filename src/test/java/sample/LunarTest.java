89482629eb8f9d329a927667860c2eac8ddb50ef
708846441 <708846441@qq.com>
2019-04-10 17:44:02 +0800

    next half way

--- src/test/java/sample/LunarTest.java
+++ src/test/java/sample/LunarTest.java
@@ -1,26 +1,21 @@
 package sample;
 
 import com.nlf.calendar.Lunar;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * 农历示例
  */
 public class LunarTest {
-
-  @Test
+  private Object[][] data = new Object[][]{
+          {1986,4,21,"1986-05-30"}
+  };
+  @org.junit.Test
   public void test(){
-    //今天
-    Lunar date = new Lunar();
-    //输出阴历信息
-    System.out.println(date.toFullString());
-    //输出阳历信息
-    System.out.println(date.getSolar().toFullString());
-    System.out.println();
-    //指定阴历的某一天
-    date = new Lunar(1986,4,21);
-    System.out.println(date.toFullString());
-    System.out.println(date.getSolar().toFullString());
+
+    Lunar date = new Lunar(Integer.parseInt(data[0][0].toString()) ,Integer.parseInt(data[0][1].toString()),Integer.parseInt(data[0][2].toString()));
+    assertEquals(data[0][3].toString(),date.getSolar().next(1).toString());
   }
 
 }

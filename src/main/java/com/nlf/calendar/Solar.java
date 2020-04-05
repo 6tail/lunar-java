18a6a0a09a128aa7e096de5c5ef1fc7166896ab2
6tail <6tail@6tail.cn>
2020-04-05 23:37:13 +0800

    修改获取星座的方法名

--- src/main/java/com/nlf/calendar/Solar.java
+++ src/main/java/com/nlf/calendar/Solar.java
@@ -174,8 +174,43 @@ public class Solar{
    * 获取星座
    *
    * @return 星座
+   * @deprecated 使用getXingZuo
    */
   public String getXingzuo(){
+    int index = 11,m = month,d = day;
+    int y = m*100+d;
+    if(y>=321&&y<=419){
+      index = 0;
+    }else if(y>=420&&y<=520){
+      index = 1;
+    }else if(y>=521&&y<=620){
+      index = 2;
+    }else if(y>=621&&y<=722){
+      index = 3;
+    }else if(y>=723&&y<=822){
+      index = 4;
+    }else if(y>=823&&y<=922){
+      index = 5;
+    }else if(y>=923&&y<=1022){
+      index = 6;
+    }else if(y>=1023&&y<=1121){
+      index = 7;
+    }else if(y>=1122&&y<=1221){
+      index = 8;
+    }else if(y>=1222||y<=119){
+      index = 9;
+    }else if(y<=218){
+      index = 10;
+    }
+    return SolarUtil.XINGZUO[index];
+  }
+
+  /**
+   * 获取星座
+   *
+   * @return 星座
+   */
+  public String getXingZuo(){
     int index = 11,m = month,d = day;
     int y = m*100+d;
     if(y>=321&&y<=419){
@@ -274,7 +309,7 @@ public class Solar{
       s.append(")");
     }
     s.append(" ");
-    s.append(getXingzuo());
+    s.append(getXingZuo());
     s.append("座");
     return s.toString();
   }

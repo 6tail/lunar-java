e06c7a0532d225a028eaf0c5e54cbc9b7ee04554
6tail <6tail@6tail.cn>
2020-10-17 20:03:01 +0800

    修复值月九星数组下标越界的问题。

--- src/main/java/com/nlf/calendar/Lunar.java
+++ src/main/java/com/nlf/calendar/Lunar.java
@@ -1903,8 +1903,11 @@ public class Lunar{
     }
     // 寅月起，所以需要-2
     int monthIndex = monthZhiIndex-2;
+    if(monthIndex<0){
+      monthIndex += 12;
+    }
     int index = start-monthIndex-1;
-    if(index<0){
+    while(index<0){
       index += 9;
     }
     return new NineStar(index);
